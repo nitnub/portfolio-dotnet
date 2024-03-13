68a1a35f8081b1bbb7928cb8a6f059a59d69e697
nitnub <45341924+nitnub@users.noreply.github.com>
2024-03-13 04:39:16 -0700

    remove pending migrations

--- Portfolio/obj/Debug/net8.0/PortfolioWeb.AssemblyInfo.cs
+++ Portfolio/obj/Debug/net8.0/PortfolioWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c8422fc9292508ed09eeeeab3c7c13fd9b83a77")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
